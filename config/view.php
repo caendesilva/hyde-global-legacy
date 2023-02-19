10950ec4d8b6194cb4c8b1c62a7265bf6da50137
Caen De Silva <caen@desilva.se>
2023-02-19 17:44:56 +0100

    Update view.php

--- config/view.php
+++ config/view.php
@@ -1,14 +1,5 @@
 <?php
 
 return [
-    'paths' => [
-        resource_path('views'),
-        base_path('_pages'),
-    ],
-
-    'compiled' => \Phar::running()
-        ? getcwd() : env(
-        'VIEW_COMPILED_PATH',
-        realpath(storage_path('framework/views'))
-    ),
+    'compiled' => getcwd(),
 ];
